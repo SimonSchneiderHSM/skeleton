fb2371cadda6d96f010b72bb92ebf0228a2ec6de
Heiko Mathes <heiko.mathes@gmail.com>
2017-08-09 18:01:21 +0200

    :fire: cleanup tart-js

--- server/src/frontend/helpers/routes/route-config-helper.js
+++ server/src/frontend/helpers/routes/route-config-helper.js
@@ -2,14 +2,10 @@
 
 import {
   Home,
-  
-  
   App
 } from '../../containers';
 
 
-import ProcessDefinitions from '/Users/heiko/Desktop/Projekte/BPMN/skeleton/server/node_modules/@process-engine-js/process_engine_http/dist/commonjs/frontend/container/ProcessDefinitions/ProcessDefinitions.js';
-import ProcessDefinition from '/Users/heiko/Desktop/Projekte/BPMN/skeleton/server/node_modules/@process-engine-js/process_engine_http/dist/commonjs/frontend/container/ProcessDefinition/ProcessDefinition.js';
 
 const relayQueries = require('../../relay');
 const relayPrepareParams = require('../../relay/prepareParams');
@@ -25,26 +21,6 @@ const routeSetup = {
       component: Home,
       prepareParams: null,
       queries: null
-    },
-   __PROCESS__DEFINITIONS__: {
-      key: 1,
-      name: 'processdefinitions',
-      breadcrumbIgnore: false,
-      breadcrumbName: '',
-      path: 'process/definitions',
-      component: ProcessDefinitions,
-      prepareParams: null,
-      queries: relayQueries.CatalogQueries
-    },
-   __PROCESS__DEFINITION__: {
-      key: 2,
-      name: 'processdefinition',
-      breadcrumbIgnore: false,
-      breadcrumbName: '',
-      path: 'process/definition/:id',
-      component: ProcessDefinition,
-      prepareParams: (relayPrepareParams && relayPrepareParams.prepareIDParams ? relayPrepareParams.prepareIDParams : null),
-      queries: relayQueries.CatalogQueries
     }
   },
   routePath: '/app',
